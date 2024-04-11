cf462bdc822029b11c07b9eb4bf683ea491fde2e
Evan Kaczor <evankaczor@icloud.com>
2024-04-11 14:50:08 -0400

    Working on Service

--- src/main/java/com/example/todolist/TaskModel.java
+++ src/main/java/com/example/todolist/TaskModel.java
@@ -1,27 +1,37 @@
 package com.example.todolist;
 
-import java.sql.Date;
-
+import java.time.LocalDate;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
+@Entity
 public class TaskModel {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column(name = "task")
     private String task;
-    private boolean taskCompleted;
-    private Date dateDue;
 
+    @Column(name = "task_completed")
+    private boolean taskCompleted;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "due_date")
+    private LocalDate dateDue;
+    
     public TaskModel() {
     }
 
-    public TaskModel(String task, boolean taskCompleted, Date dateDue) {
+    public TaskModel(String task, boolean taskCompleted, String category, LocalDate dateDue) {
         this.task = task;
         this.taskCompleted = taskCompleted;
+        this.category = category;
         this.dateDue = dateDue;
     }
 
@@ -49,12 +59,20 @@ public class TaskModel {
         this.taskCompleted = taskCompleted;
     }
 
-    public Date getDateDue() {
+    public LocalDate getDateDue() {
         return dateDue;
     }
 
-    public void setDateDue(Date dateDue) {
+    public void setDateDue(LocalDate dateDue) {
         this.dateDue = dateDue;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
 }
