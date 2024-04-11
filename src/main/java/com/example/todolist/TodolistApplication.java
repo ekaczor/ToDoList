64064f8643acf1f56716cc96ea9a1bce626241ea
Evan Kaczor <evankaczor@icloud.com>
2024-04-10 20:53:45 -0400

    Git Success Test

--- src/main/java/com/example/todolist/TodolistApplication.java
+++ src/main/java/com/example/todolist/TodolistApplication.java
@@ -8,7 +8,7 @@ public class TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
-		System.out.println("Goodbye World");
+		System.out.println("Goodbye World, apsdfoij");
 	}
 
 }
