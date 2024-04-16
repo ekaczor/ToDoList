1690d4514d0956ed072d6ad09fb4730398858f0e
Evan Kaczor <evankaczor@icloud.com>
2024-04-15 22:13:49 -0400

    Worked on the Styling for the tasks

--- my-todo-app/src/components/TaskBoard.js
+++ my-todo-app/src/components/TaskBoard.js
@@ -1,9 +1,17 @@
 import './TaskBoard.css';
+import Task from './Task';
+import React from 'react';
 
 const TaskBoard = () => {
 return (
 <div className='TaskBoard'>
+<div className='above'>
 <h1 className="title">TaskTide</h1>
+<button className='button'>Add Task</button>
+</div>
+<div className='tasks'>
+    <Task taskCompleted='false' task='Do the dishes' category='House' dateDue='2024-04-20'/>
+</div>
 </div>
 )}
 
