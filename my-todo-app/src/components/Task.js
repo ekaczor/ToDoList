bf13767af8d31f75c99a59bbaee40678c8e528f4
Evan Kaczor <evankaczor@icloud.com>
2024-04-17 12:03:57 -0400

    Improved UI

--- my-todo-app/src/components/Task.js
+++ my-todo-app/src/components/Task.js
@@ -1,16 +1,23 @@
 import React,  {useState} from 'react';
 import './Task.css'
+import axios from 'axios';
 
 const Task = ({task, taskCompleted, category, dateDue}) => {
 
-    // const [ , ] = useState
+    const handleEdit = () => console.log("Edit");
+
+    const handleDelete = () => console.log("Delete");
 
     return(
         <div className='task'>
-        <div>{taskCompleted}</div>
-        <div>{task}</div>
-        <div>{category}</div>
-        <div>{dateDue}</div>
+        <div class='contents'>{taskCompleted}</div>
+        <div class='contents'>{task}</div>
+        <div class='contents'>{category}</div>
+        <div class='contents'>{dateDue}</div>
+        <div class='edit_delete'>
+        <div class='edit' onClick={handleEdit}>Edit</div>
+        <div class='delete' onClick={handleDelete}>Delete</div>
+        </div>
         </div>
     )
 }
--- my-todo-app/src/components/TaskBoard.js
+++ my-todo-app/src/components/TaskBoard.js
@@ -3,11 +3,14 @@ import Task from './Task';
 import React from 'react';
 
 const TaskBoard = () => {
+
+const handleAddTask = () => console.log("Add Task");
+
 return (
 <div className='TaskBoard'>
 <div className='above'>
 <h1 className="title">TaskTide</h1>
-<button className='button'>Add Task</button>
+<button className='button' onClick={handleAddTask}>Add Task</button>
 </div>
 <div className='tasks'>
     <Task taskCompleted='false' task='Do the dishes' category='House' dateDue='2024-04-20'/>
