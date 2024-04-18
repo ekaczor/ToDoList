7535464d23aed2ae473325891c705d51fdf2a3f6
Evan Kaczor <evankaczor@icloud.com>
2024-04-18 14:25:20 -0400

    Add Task Modal improved

--- my-todo-app/src/components/AddTaskModal.js
+++ my-todo-app/src/components/AddTaskModal.js
@@ -1,14 +1,39 @@
-import "./AddTaskModal.css"
+import React from "react";
+import "./AddTaskModal.css";
 
-const AddTaskModal = ({onClose}) => {
-    return (
-        <div className="modal">
-            <div className="top">
-            <div>Add Task</div>
-            <div className="exit" onClick={onClose}>X</div>
-            </div>
-        </div>
-    )
-}
+const AddTaskModal = ({ onClose }) => {
+  return (
+    <div className="modal">
+      <div className="exit" onClick={onClose}>
+        X
+      </div>
+      <div className="top">
+        <div className="titleModal">Add Task</div>
+      </div>
+      <div className="userInput">
+        <label htmlFor="task">
+          Task Description
+          <input
+            type="text"
+            placeholder="ex. File your taxes"
+            required
+            maxLength="100"
+          />
+        </label>
+        <label htmlFor="category">
+          Category
+          <input type="text" placeholder="ex. Home" required maxLength="50" />
+        </label>
+        <label htmlFor="dateDue">
+          Date Due
+          <input type="date" />
+        </label>
+      </div>
+      <button className="finishButton" onClick={onClose}>
+        Finish Task
+      </button>
+    </div>
+  );
+};
 
 export default AddTaskModal;
